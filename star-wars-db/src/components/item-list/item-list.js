848d5d423eb82d23595b9725d029e3df11ae5542
Aliaksandr Shliayeu <aliaksandr_shliayeu@epam.com>
2019-06-21 09:03:46 +0300

    propTypes & reactRouter

--- star-wars-db/src/components/item-list/item-list.js
+++ star-wars-db/src/components/item-list/item-list.js
@@ -1,28 +1,42 @@
-import React, { Component } from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { withData } from "../hoc-helpers";
-import SwapiService from "../../services/swapi-service";
-import "./item-list.css";
+import { withData } from '../hoc-helpers';
+import SwapiService from '../../services/swapi-service';
+import './item-list.css';
 
-const ItemList = props => {
+const ItemList = (props) => {
+
   const { data, onItemSelected, children: renderLabel } = props;
 
-  const items = data.map(item => {
+  const items = data.map((item) => {
     const { id } = item;
     const label = renderLabel(item);
 
     return (
-      <li
-        className="list-group-item"
-        key={id}
-        onClick={() => onItemSelected(id)}
-      >
+      <li className="list-group-item"
+          key={id}
+          onClick={() => onItemSelected(id)}>
         {label}
       </li>
     );
   });
 
-  return <ul className="item-list list-group">{items}</ul>;
+  return (
+    <ul className="item-list list-group">
+      {items}
+    </ul>
+  );
+};
+
+ItemList.defaultProps = {
+  onItemSelected: () => {}
+};
+
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired
 };
 
 const { getAllPeople } = new SwapiService();
