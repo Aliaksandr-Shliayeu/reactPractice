13e570761d072fe5efb7abcf1272224452df28e6
Aliaksandr Shliayeu <shlyaeu@gmail.com>
2019-07-05 13:39:57 +0300

    updates buttons

--- ReStore/src/actions/index.js
+++ ReStore/src/actions/index.js
@@ -1,13 +1,13 @@
+const booksRequested = () => {
+  return {
+    type: "FETCH_BOOKS_REQUEST"
+  };
+};
+
 const booksLoaded = newBooks => {
   return {
     type: "FETCH_BOOKS_SUCCESS",
     payload: newBooks
-  };
-};
-
-const booksRequested = newBooks => {
-  return {
-    type: "FETCH_BOOKS_REQUEST"
   };
 };
 
@@ -18,9 +18,23 @@ const booksError = error => {
   };
 };
 
-const bookAddedToCart = bookId => {
+export const bookAddedToCart = bookId => {
   return {
     type: "BOOK_ADDED_TO_CART",
+    payload: bookId
+  };
+};
+
+export const bookRemovedFromCart = bookId => {
+  return {
+    type: "BOOK_REMOVED_FROM_CART",
+    payload: bookId
+  };
+};
+
+export const allBooksRemovedFromCart = bookId => {
+  return {
+    type: "ALL_BOOKS_REMOVED_FROM_CART",
     payload: bookId
   };
 };
@@ -30,7 +44,7 @@ const fetchBooks = (bookstoreService, dispatch) => () => {
   bookstoreService
     .getBooks()
     .then(data => dispatch(booksLoaded(data)))
-    .catch(error => dispatch(booksError(error)));
+    .catch(err => dispatch(booksError(err)));
 };
 
-export { fetchBooks, bookAddedToCart };
+export { fetchBooks };
